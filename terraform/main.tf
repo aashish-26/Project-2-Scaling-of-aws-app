92c58617f578642865265d68fa2f63e2d34a1061
aashish-26 <abhandari.2002@gmail.com>
2025-06-30 23:14:19 +0530

    Final commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,44 +15,44 @@ module "alb" {
 }
 
 module "ec2" {
-  source              = "./modules/ec2-asg"
-  project_name        = var.project_name
-  vpc_id              = module.vpc.vpc_id
-  private_subnet_ids  = module.vpc.private_subnet_ids
-  alb_sg_id           = module.alb.alb_sg_id
-  target_group_arn    = module.alb.target_group_arn
-  ami_id              = var.ami_id
-  user_data           = filebase64("user_data.sh")
+  source             = "./modules/ec2-asg"
+  project_name       = var.project_name
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+  alb_sg_id          = module.alb.alb_sg_id
+  target_group_arn   = module.alb.target_group_arn
+  ami_id             = var.ami_id
+  user_data          = filebase64("user_data.sh")
 }
 
 module "rds" {
-  source              = "./modules/rds"
-  project_name        = var.project_name
-  vpc_id              = module.vpc.vpc_id
-  private_subnet_ids  = module.vpc.private_subnet_ids
-  ec2_sg_id           = module.ec2.app_sg_id
-  db_username         = var.db_username
-  db_password         = var.db_password
+  source             = "./modules/rds"
+  project_name       = var.project_name
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+  ec2_sg_id          = module.ec2.app_sg_id
+  db_username        = var.db_username
+  db_password        = var.db_password
 }
 
 module "redis" {
-  source              = "./modules/redis"
-  project_name        = var.project_name
-  vpc_id              = module.vpc.vpc_id
-  private_subnet_ids  = module.vpc.private_subnet_ids
-  ec2_sg_id           = module.ec2.app_sg_id
+  source             = "./modules/redis"
+  project_name       = var.project_name
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+  ec2_sg_id          = module.ec2.app_sg_id
 }
 
 module "s3_cloudfront" {
-  source        = "./modules/s3-cloudfront"
-  project_name  = var.project_name
+  source       = "./modules/s3-cloudfront"
+  project_name = var.project_name
 }
 
 module "monitoring" {
-  source        = "./modules/monitoring"
+  source = "./modules/monitoring"
 
-  project_name  = var.project_name
-  vpc_id        = module.vpc.vpc_id
-  asg_name      = module.ec2.asg_name
-  alert_email   = var.alert_email
+  project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
+  asg_name     = module.ec2.asg_name
+  alert_email  = var.alert_email
 }
